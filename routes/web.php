6801b6309b229001845d3ee9b9d03403d92c7746
Guess-dls <ninopaket@gmail.com>
2025-11-06 01:56:15 +0000

    Actualisation du projet

--- routes/web.php
+++ routes/web.php
@@ -95,8 +95,8 @@ Route::get('/reservation/{id}/rebook', [ReservationController::class, 'rebook'])
 
 Route::post('residences/{id}/activation', [AdminController::class, 'activation'])->name('admin.residences.activation');
 
-Route::post('residences/{id}/update', [AdminController::class, 'residence_update'])->name('admin.residences.update');
-
+// Route PUT pour la mise à jour
+Route::put('/residences/{residence}/update', [ResidenceController::class, 'update'])->name('admin.residences.update');
 
 Route::post('residences/{id}/desactivation', [AdminController::class, 'desactivation'])->name('admin.residences.desactivation');
 
