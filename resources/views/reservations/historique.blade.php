b00b45cb1cdc49069c2c20396dfd50abf0d359df
Guess-dls <ninopaket@gmail.com>
2025-11-09 17:04:08 +0000

    Actualisation du projet

--- resources/views/reservations/historique.blade.php
+++ resources/views/reservations/historique.blade.php
@@ -89,7 +89,7 @@
                         <!-- Boutons en bas -->
                         <div class="mt-4 flex gap-2 justify-center">
                             @if($res->status != 'payé')
-                                <form action="{{ route('payer', $res->id) }}" method="GET" class="flex-1">
+                                <form action="{{ route('payer', $res->id) }}" method="POST" class="flex-1">
                                     @csrf
                                     <button type="submit" class="w-full py-2 bg-amber-500 text-white font-semibold rounded-lg hover:bg-amber-600 transition duration-150 shadow-md text-sm">
                                         <i class="fas fa-credit-card mr-1"></i> Payer
