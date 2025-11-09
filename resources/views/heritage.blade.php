76bb18b3132804a8c51f7f1efc3d9f4d35ccc01b
Guess-dls <ninopaket@gmail.com>
2025-11-09 13:06:05 +0000

    Actualisation du projet

--- resources/views/heritage.blade.php
+++ resources/views/heritage.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @yield('titre')
+        <title>@yield('titre')</title>
 
         <!-- GLightbox CSS (pour le carrousel d'images) -->
         <link href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" rel="stylesheet">
