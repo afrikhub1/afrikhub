79454c45edce6b7a7eefa79fd6805cc5282040f7
Guess-dls <ninopaket@gmail.com>
2025-11-10 10:06:12 +0000

    Actualisation du projet

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -6,8 +6,8 @@
   <title>Détails de la résidence</title>
 
   <!-- Icônes / Bootstrap / Font -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome-free-6.4.0-web/css/all.css') }}">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
 
   <!-- GLightbox (lightbox léger et mobile-friendly) -->
