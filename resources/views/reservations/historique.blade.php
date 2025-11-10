1c2ec990acc54196483b45276bebe00f4615bb3c
Guess-dls <ninopaket@gmail.com>
2025-11-10 03:16:35 +0000

    Actualisation du projet

--- resources/views/reservations/historique.blade.php
+++ resources/views/reservations/historique.blade.php
@@ -145,7 +145,7 @@
         </div>
 
         @if(session('success'))
-            <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 rounded-lg mb-8 shadow-md">
+            <div class="bg-green-100 border-l-4 border-green-500 text-green-700 text-center p-4 rounded-lg mb-8 shadow-md">
                 {{ session('success') }}
             </div>
         @endif
