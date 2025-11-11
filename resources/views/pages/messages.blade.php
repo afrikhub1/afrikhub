05d7f960533dfca35ad594ea87d9273d1e7212fb
Guess-dls <ninopaket@gmail.com>
2025-11-11 02:15:36 +0000

    Actualisation du projet

--- resources/views/pages/messages.blade.php
+++ resources/views/pages/messages.blade.php
@@ -4,7 +4,10 @@
 <head>
     <meta charset="UTF-8" />
     <title>Message</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome-free-6.4.0-web/css/all.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
     <style>
         body, html {
             height: 100%;
