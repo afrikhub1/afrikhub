2a06b4f6f983e222640560d3e135673c2b106206
Guess-dls <ninopaket@gmail.com>
2025-11-11 02:13:51 +0000

    Actualisation du projet

--- resources/views/pages/messages.blade.php
+++ resources/views/pages/messages.blade.php
@@ -37,6 +37,12 @@
             </div>
         @endif
 
+        @if(session('info'))
+            <div class="alert alert-info shadow-sm rounded">
+                {{ session('success') }}
+            </div>
+        @endif
+
         @if(session('error'))
             <div class="alert alert-danger shadow-sm rounded">
                 {{ session('error') }}
