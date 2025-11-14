60eaa39554c211b0f44db355ba9eb3ff402e82ab
Guess-dls <ninopaket@gmail.com>
2025-11-14 05:53:39 +0000

    Actualisation du projet

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -14,7 +14,7 @@
         <nav class="col-lg-10 col-md-10 col-9">
           <ul class="d-flex justify-content-end py-2">
             <ul id="entete">
-               <li><a href="{{ route('dashboard') }}" class="bg-dark" aria-label="inscription"><span class="fa fa-sign-in"></span><span class="badge">connectiton</span></a></li>
+               <li><a href="{{ route('login') }}" class="bg-dark" aria-label="inscription"><span class="fa fa-sign-in"></span><span class="badge">connectiton</span></a></li>
               <li><a href="{{ route('register') }}" class="bg-dark" aria-label="inscription"><span class="fa fa-sign-in"></span><span class="badge">inscription</span></a></li>
               <li><a href="{{ route('admin_dashboard') }}" class="bg-danger"><span class="fa fa-user-shield"></span><span class="badge">admin</span></a></li>
               <li><a href="#hebergement"><span class="fa fa-home"></span><span class="badge">herbergement</span></a></li>
@@ -26,7 +26,7 @@
               <ul class="dropdown-menu row m-0 py-2 col-8 col-md-6" aria-label="submenu">
                 <li><a class="dropdown-item" href="{{ route('login') }}">connexion</a></li>
                 <li><a class="dropdown-item" href="{{ route('register') }}">inscription</a></li>
-                <li><a class="dropdown-item" href="../../admins/php/admin_connect.php">admin</a></li>
+                <li><a class="dropdown-item" href="{{ route('admin_dashboard') }}">admin</a></li>
                 <li><a class="dropdown-item" href="#hebergement">Hébergements</a></li>
                 <li><a class="dropdown-item" href="#location">Véhicules</a></li>
                 <li><a class="dropdown-item" href="#circuits">Circuits</a></li>
