8e7b6ac4f212237446c37d350d45583f1ae791ef
Guess-dls <ninopaket@gmail.com>
2025-11-14 06:02:41 +0000

    Actualisation du projet

--- resources/views/pages/heritage_pages.blade.php
+++ resources/views/pages/heritage_pages.blade.php
@@ -108,11 +108,11 @@
             </div>
         </header>
 
-        <div>
-            <a href="{{ route('recherche')}}"><i class="fas fa-search mr-1"></i> Recherche</a>
-        </div>
-        <!-- Sidebar (Off-Canvas Menu) -->
+        <!-- Sidebar -->
         <div id="sidebar" class="text-white flex flex-col items-center">
+
+            <a href="{{ route('recherche')}}"><i class="fas fa-search mr-1"></i> Recherche</a>
+
             <button id="closeSidebar" class="absolute top-4 right-4 text-gray-400 hover:text-white transition">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
             </button>
@@ -123,7 +123,7 @@
                 </a>
 
                 <a href="{{ route('recherche') }}" class="w-full text-center py-2 px-4 rounded-lg hover:bg-gray-700 transition">
-                    Recherche
+                    <i class="fas fa-search mr-1"></i>Recherche
                 </a>
 
                 <a href="{{ route('historique') }}" class="w-full text-center py-2 px-4 rounded-lg hover:bg-gray-700 transition">
