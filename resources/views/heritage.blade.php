e7ef003b45242e467e0c2480d16a3c5d5ced14df
Guess-dls <ninopaket@gmail.com>
2025-11-15 19:00:32 +0000

    Actualisation du projet

--- resources/views/heritage.blade.php
+++ resources/views/heritage.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/fontawesome-free-6.4.0-web/css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/accueil.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
