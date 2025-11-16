ffdb923786c20d16d6db82d2a96a22aef1d6cb9c
Guess-dls <ninopaket@gmail.com>
2025-11-16 18:10:13 +0000

    Actualisation du projet

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -177,7 +177,7 @@
                                 </ul>
 
                                 <!-- Badge Statut -->
-                                @if(isset($res->status) && $res->status === 'occupee')
+                                @if($res->status == 1)
                                     <span class="bg-red-500 w-full p-3 text-white font-bold rounded-xl text-center
                                                 shadow-lg transition duration-150">
                                         <i class="fas fa-bed mr-2"></i> Déjà Occupée
