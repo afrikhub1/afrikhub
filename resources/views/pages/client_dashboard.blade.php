5c4aee6e35a219958d799cc6063fb009f61cf744
Guess-dls <ninopaket@gmail.com>
2025-11-16 17:15:48 +0000

    Actualisation du projet

--- resources/views/pages/client_dashboard.blade.php
+++ resources/views/pages/client_dashboard.blade.php
@@ -135,7 +135,7 @@
                         <i class="fas fa-credit-card"></i> Payer
                         </a>
                     @elseif ($status=='payé')
-                        <button disabled class="inline-flex items-center justify-center gap-2 rounded-md bg-slate-100 px-3 py-2 text-sm font-semibold text-slate-400 cursor-not-allowed">
+                        <button disabled class="block items-center justify-center gap-2 rounded-md bg-green-100 px-3 py-2 text-sm font-semibold text-slate-400 cursor-not-allowed">
                         <i class="fas fa-credit-card"></i> Payé
                         </button>
                     @endif
