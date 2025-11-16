701491b50e77a5292cdd291cac0a15aaf9638bb3
Guess-dls <ninopaket@gmail.com>
2025-11-16 09:16:54 +0000

    Actualisation du projet

--- resources/views/pages/heritage_pages.blade.php
+++ resources/views/pages/heritage_pages.blade.php
@@ -121,7 +121,7 @@
                 Recherche
             </a>
 
-            <a href="{{ route('historique') }}" class="w-full text-center py-2 hover:bg-gray-700 rounded-lg flex items-center justify-center gap-2">
+            <a href="{{ route('reservationRecu') }}" class="w-full text-center py-2 hover:bg-gray-700 rounded-lg flex items-center justify-center gap-2">
                 <!-- Clock Rotate Left -->
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
