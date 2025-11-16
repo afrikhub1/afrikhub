246fbdbaf741d4e13317c9b4666c2997b79d27d1
Guess-dls <ninopaket@gmail.com>
2025-11-16 08:53:18 +0000

    Actualisation du projet

--- resources/views/pages/heritage_pages.blade.php
+++ resources/views/pages/heritage_pages.blade.php
@@ -67,22 +67,22 @@
 
                 <a href="{{ route('residences') }}" class="flex-1 min-w-[25%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
                     <i class="fas fa-home mr-1"></i> Résidences
-                    <span class="ml-1 px-2 bg-red-600 text-xs font-bold rounded-full">{{ $totalResidences->count() }}</span>
+                    <span class="ml-1 px-2 bg-red-600 text-xs font-bold rounded-full">{{ $totalResidences}}</span>
                 </a>
 
                 <a href="{{ route('occupees') }}" class="flex-1 min-w-[25%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
                     <i class="fas fa-lock mr-1"></i> Occupées
-                    <span class="ml-1 px-2 bg-yellow-500 text-xs font-bold rounded-full">{{ $residencesOccupees->count() }}</span>
+                    <span class="ml-1 px-2 bg-yellow-500 text-xs font-bold rounded-full">{{ $totalResidencesOccupees}}</span>
                 </a>
 
                 <a href="{{ route('mes_demandes') }}" class="flex-1 min-w-[25%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
                     <i class="fas fa-spinner mr-1"></i> Demandes
-                    <span class="ml-1 px-2 bg-gray-600 text-xs font-bold rounded-full">{{ $demandesEnAttente->count() }}</span>
+                    <span class="ml-1 px-2 bg-gray-600 text-xs font-bold rounded-full">{{ $totalDemandesEnAttente}}</span>
                 </a>
 
                 <a href="{{ route('historique') }}" class="flex-1 min-w-[25%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
                     <i class="fas fa-clock mr-1"></i> Historique
-                    <span class="ml-1 px-2 bg-green-600 text-xs font-bold rounded-full">{{ $reservationsRecu->count() }}</span>
+                    <span class="ml-1 px-2 bg-green-600 text-xs font-bold rounded-full">{{ $totalReservationsReçu}}</span>
                 </a>
 
             </div>
