2f34880c0826a7c25c796ffd85aa084eba9b777c
Guess-dls <ninopaket@gmail.com>
2025-11-17 16:06:54 +0000

    Actualisation du projet

--- resources/views/includes/admin_header.blade.php
+++ resources/views/includes/admin_header.blade.php
@@ -7,15 +7,8 @@
                 </div>
                 <h1 class="text-xl font-semibold text-white">gestionnaire@afrikhub.com</h1>
             </div>
-            <button id="toggleSidebar" class="p-2 rounded-lg text-white hover:bg-indigo-700 focus:outline-none transition duration-150">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                </svg>
-            </button>
-        </div>
 
-        <div class="flex items-center space-x-2 py-3 border-t border-gray-800">
-            <div class="relative flex-grow">
+             <div class="relative flex-grow">
                 <input type="text" id="searchInput" placeholder="Rechercher par nom ou statut de résidence..."
                        class="w-full py-2 pl-10 pr-4 bg-gray-800 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-150"
                        onkeyup="filterElements()">
@@ -27,6 +20,16 @@
                 <option value="name">Nom de la Résidence</option>
                 <option value="all">Tout le Contenu</option>
             </select>
+            
+            <button id="toggleSidebar" class="p-2 rounded-lg text-white hover:bg-indigo-700 focus:outline-none transition duration-150">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                </svg>
+            </button>
+        </div>
+
+        <div class="flex items-center space-x-2 py-3 border-t border-gray-800">
+
         </div>
         <div class="flex flex-wrap justify-between text-center border-t border-gray-800 py-2 -mx-4">
             <a href="{{ route('admin.dashboard') }}" class="flex-1 min-w-[15%] p-2 text-sm md:text-base font-medium text-gray-300 hover:bg-gray-800 transition duration-150 rounded-lg">
