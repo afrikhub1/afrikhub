c6f9d49ab21b5a69f0cd6936f8bbcfb021da00fd
Guess-dls <ninopaket@gmail.com>
2025-11-17 15:59:48 +0000

    Actualisation du projet

--- app/Models/InterruptionRequest.php
+++ app/Models/InterruptionRequest.php
@@ -8,13 +8,18 @@ class InterruptionRequest extends Model
 {
     protected $fillable = ['user_id', 'residence_id', 'status'];
 
+    // relation entre les tables
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-
     public function residence()
     {
         return $this->belongsTo(Residence::class);
     }
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
 }
+
