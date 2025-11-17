403c423bb20b52737b688f9f2648b071cedc5cb3
Guess-dls <ninopaket@gmail.com>
2025-11-17 14:46:41 +0000

    Actualisation du projet

--- resources/views/pages/client_dashboard.blade.php
+++ resources/views/pages/client_dashboard.blade.php
@@ -72,6 +72,8 @@
       </a>
     </div>
 
+     @include('includes.messages')
+
     {{-- Grid de cartes --}}
     <section>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
