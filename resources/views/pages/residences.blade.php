637aeb2e248ef3b8762739c158bb0ee83d3af7f6
Guess-dls <ninopaket@gmail.com>
2025-11-17 08:35:17 +0000

    Actualisation du projet

--- resources/views/pages/residences.blade.php
+++ resources/views/pages/residences.blade.php
@@ -4,7 +4,7 @@
 
 @section('main')
     <!-- Main Content Area (Ajusté pour le Header) -->
-    <div class="container mx-auto p-2 mt-4">
+    <div class="container-fluid mx-4 px-4 py-2 mt-2">
 
         <!-- Titre Principal de la Page -->
         <h1 class="text-3xl lg:text-4xl font-extrabold text-indigo-700 mb-8 text-center border-b-4 border-indigo-500 pb-3">
