80440c5b94807caf59d879069075535a60e12a95
Guess-dls <ninopaket@gmail.com>
2025-11-17 12:42:24 +0000

    Actualisation du projet

--- resources/views/pages/heritage_pages.blade.php
+++ resources/views/pages/heritage_pages.blade.php
@@ -61,14 +61,14 @@
                 </div>
 
                                 <!-- Bouton Ajouter une résidence -->
-                <a href="{{ route('mise_en_ligne') }}" class="w-full text-center py-2 px-4 hover:bg-gray-700 rounded-lg flex items-center justify-center gap-2 text-white mt-2">
+                <a href="{{ route('mise_en_ligne') }}" class="w-full text-center py-2 px-4 hover:bg-gray-700 rounded-lg flex items-center justify-center gap-2 text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
                     </svg>
                     Ajouter
                 </a>
 
-                <a href="{{ route('clients_historique')}}" class="w-full text-center py-2 px-4 hover:bg-gray-700 rounded-lg flex items-center justify-center gap-2 text-white mt-2">
+                <a href="{{ route('clients_historique') }}" class="w-full text-center py-2 px-4 hover:bg-gray-700 rounded-lg flex items-center justify-center gap-2 text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                     </svg>
