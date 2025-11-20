fc042b85c4c22051f09e5044745a9361e7094fa2
Guess-dls <ninopaket@gmail.com>
2025-11-20 21:56:42 +0000

    Actualisation du projet

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -51,7 +51,7 @@ class ForgotPasswordController extends Controller
     // Afficher le formulaire de réinitialisation
     public function showResetForm(Request $request, $token)
     {
-        return view('auth.reset-password', [
+        return view('mails.reset-password', [
             'token' => $token,
             'email' => $request->email,
         ]);
