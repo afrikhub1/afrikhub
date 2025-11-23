feedf639de8759082d8cac298e99d3e47ac50395
Guess-dls <ninopaket@gmail.com>
2025-11-23 10:42:04 +0000

    Actualisation du projet

--- resources/views/reservations/occupees.blade.php
+++ resources/views/reservations/occupees.blade.php
@@ -6,7 +6,7 @@
     <div class="container-fluid px-2 py-2 mt-2">
 
         <main class="bg-white p-6 md:p-8 rounded-xl shadow-2xl border border-gray-200">
-            <h1 class="text-xl font-extrabold text-red-600 mb-8 text-center border-b-4 border-red-500 pb-3">
+            <h1 class="text-2xl font-extrabold text-red-600 mb-8 text-center border-b-4 border-red-500 pb-3">
                 <i class="fas fa-lock-open text-3xl mr-3"></i> Résidences Occupées
             </h1>
 
@@ -22,7 +22,7 @@
                         @foreach($residences_occupees as $residences_occupees)
                             <div class="w-full sm:w-[320px] bg-red-50 border-2 border-red-400 rounded-xl shadow-2xl p-6 flex flex-col justify-between">
                                 <div>
-                                    <h5 class="text-2xl font-bold text-red-800 mb-3 flex items-center">
+                                    <h5 class="text-xl font-bold text-red-800 mb-3 flex items-center">
                                         <i class="fas fa-building mr-3 text-red-600"></i> {{ $residences_occupees->nom }}
                                     </h5>
                                     <p class="text-sm mb-2"><strong>Ville :</strong> {{ $residences_occupees->ville }}</p>
