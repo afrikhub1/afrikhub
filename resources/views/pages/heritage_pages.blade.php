7873b20fa79598686e0563a933335390c8724908
Guess-dls <ninopaket@gmail.com>
2025-11-23 09:25:34 +0000

    Actualisation du projet

--- resources/views/pages/heritage_pages.blade.php
+++ resources/views/pages/heritage_pages.blade.php
@@ -162,22 +162,22 @@
             <!-- Ligne des statistiques -->
             <div class="flex flex-wrap justify-between text-center border-t border-gray-800 py-2 -mx-4">
 
-                <a href="{{ route('pro.residences') }}" class="flex-1 min-w-[25%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
+                <a href="{{ route('pro.residences') }}" class="flex-1 min-w-[20%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
                     <i class="fas fa-home mr-1"></i> Résidences
                     <span class="ml-1 px-2 bg-red-600 text-xs font-bold rounded-full">{{ $totalResidences }}</span>
                 </a>
 
-                <a href="{{ route('occupees') }}" class="flex-1 min-w-[25%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
+                <a href="{{ route('occupees') }}" class="flex-1 min-w-[20%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
                     <i class="fas fa-lock mr-1"></i> Occupées
                     <span class="ml-1 px-2 bg-yellow-500 text-xs font-bold rounded-full">{{ $totalResidencesOccupees }}</span>
                 </a>
 
-                <a href="{{ route('mes_demandes') }}" class="flex-1 min-w-[25%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
+                <a href="{{ route('mes_demandes') }}" class="flex-1 min-w-[20%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
                     <i class="fas fa-spinner mr-1"></i> Demandes
                     <span class="ml-1 px-2 bg-gray-600 text-xs font-bold rounded-full">{{ $totalDemandesEnAttente }}</span>
                 </a>
 
-                <a href="{{ route('reservationRecu') }}" class="flex-1 min-w-[25%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
+                <a href="{{ route('reservationRecu') }}" class="flex-1 min-w-[20%] p-2 text-gray-300 hover:bg-gray-800 rounded-lg">
                     <i class="fas fa-clock mr-1"></i> Historique
                     <span class="ml-1 px-2 bg-green-600 text-xs font-bold rounded-full">{{ $totalReservationsRecu }}</span>
                 </a>
