1b906b03b3596ddf39f2bd872983f09abf2373bf
Guess-dls <ninopaket@gmail.com>
2025-12-07 14:34:32 +0000

    Actualisation du projet

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -423,12 +423,6 @@
             <section id="hebergement" class="my-8 px-4 py-6 bg-teal-50 rounded-2xl shadow-lg">
                 <h2 class="text-3xl font-extrabold text-center text-teal-800 uppercase mb-8">Hébergements</h2>
 
-                <div class="flex justify-center mb-6">
-                    <div class="w-full md:w-2/3 lg:w-1/2">
-                        <img class="w-full h-auto rounded-xl shadow-md object-cover" src="{{ asset('assets/images/hebergement.jpg') }}" alt="Image de résidences"/>
-                    </div>
-                </div>
-
                 <div class="accordion-css mx-auto max-w-2xl">
                     <input type="checkbox" id="acc1" checked>
                     <label for="acc1">
@@ -512,11 +506,6 @@
                                 <input id="searchInput" type="text"
                                     placeholder="Rechercher par nom ou status..."
                                     class="w-full py-2 pl-10 pr-4 bg-gray-800 text-white rounded-lg focus:ring-2 focus:ring-indigo-500">
-                                <svg class="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400"
-                                    fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                </svg>
                             </div>
 
                             <!-- OPTION -->
