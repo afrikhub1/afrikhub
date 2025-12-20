a326165f16523a144664f58d55ae30cbee8ec20d
Guess-dls <ninopaket@gmail.com>
2025-12-20 11:01:55 +0000

    Actualisation du projet

--- app/Models/carousel.php
+++ app/Models/carousel.php
@@ -1,12 +1,11 @@
 <?php
-// app/Models/Carousel.php
 
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Carousel extends Model 
+class Carousel extends Model
 {
     use HasFactory;
 
