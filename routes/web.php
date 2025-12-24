955cfad98a8f4f5e96311446fad9c0593f0cfaa9
Guess-dls <ninopaket@gmail.com>
2025-12-24 13:25:56 +0000

    Mise à jour du panier et suivi de commande

--- routes/web.php
+++ routes/web.php
@@ -91,7 +91,7 @@ Route::middleware(['auth'])->group(function () {
 
 
         // Route pour la page des QR codes
-        Route::get('/qrcodes', function () { return view('paiement.paiement')->name('qrcodes');
+        Route::get('/paiement', function () { return view('paiement.paiement')->name('qrcodes');
 
 
     });
