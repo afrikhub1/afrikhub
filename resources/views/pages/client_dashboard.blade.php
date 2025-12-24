b29741fd85244677f336108d74234c24954aa37d
Guess-dls <ninopaket@gmail.com>
2025-12-24 15:00:00 +0000

    Mise à jour du panier et suivi de commande

--- resources/views/pages/client_dashboard.blade.php
+++ resources/views/pages/client_dashboard.blade.php
@@ -139,6 +139,7 @@
               </form>
               @elseif($res->status=='confirmée')
                 <button disabled class="w-full px-3 py-2 bg-green-100 text-slate-400 rounded-lg">
+                    {{-- <a href="{{ route('payer', $res->id) }}" class="d-flex">payer</a> --}}
                     <a href="{{ route('payer', $res->id) }}" class="d-flex">payer</a>
                 </button>
               @elseif($res->status=='payé')
