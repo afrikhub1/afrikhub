3e4dd9e7c2237908c19fc6c51a52e9675228ce87
Guess-dls <ninopaket@gmail.com>
2025-12-24 14:19:02 +0000

    Mise à jour du panier et suivi de commande

--- database/migrations/2025_12_24_121601_create_newsletters_table.php
+++ database/migrations/2025_12_24_121601_create_newsletters_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('contacts', function (Blueprint $table) {
+        Schema::create('news_letters', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
             $table->string('email');
