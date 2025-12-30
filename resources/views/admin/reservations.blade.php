985d3899107ae25aa5c6dd1e7201002ecf807bbf
Guess-dls <ninopaket@gmail.com>
2025-12-30 11:41:47 +0000

    Mise à jour du panier et suivi de commande

--- resources/views/admin/reservations.blade.php
+++ resources/views/admin/reservations.blade.php
@@ -110,6 +110,7 @@
 
                                     @php
                                         $color = match ($reservation->status) {
+                                            'payée' => 'bg-indigo-100 text-indigo-800',
                                             'confirmée' => 'bg-green-100 text-green-800',
                                             'annulée' => 'bg-red-100 text-red-800',
                                             default => 'bg-gray-100 text-gray-800',
