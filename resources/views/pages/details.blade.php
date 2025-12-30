904c2b6e6be70aa1f7b72fa9ed3683d07dcd97c4
Guess-dls <ninopaket@gmail.com>
2025-12-30 00:07:50 +0000

    Mise à jour du panier et suivi de commande

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -329,11 +329,11 @@
                     <a class="btn btn-outline-dark" href="{{ route('recherche') }}" aria-label="Retour aux résidences">⬅ Retour</a>
 
                     @auth
-                        <button class="btn btn-reserver" data-bs-toggle="modal" data-bs-target="#reservationModal" id="openReserve">
+                        <button class="btn btn-reserver btn-outline-success" data-bs-toggle="modal" data-bs-target="#reservationModal" id="openReserve">
                             Réserver
                         </button>
                     @else
-                        <button class="btn btn-reserver" id="btnReserveGuest">
+                        <button class="btn btn-reserver btn-outline-success" id="btnReserveGuest">
                             Se connecter pour réserver
                         </button>
                     @endauth
