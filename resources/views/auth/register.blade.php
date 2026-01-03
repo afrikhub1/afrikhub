0adbe478cc9a0055c34fde5fb373ca24c6b49524
Guess-dls <ninopaket@gmail.com>
2026-01-03 22:15:16 +0000

    Mise à jour du panier et suivi de commande

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -185,7 +185,7 @@
                     <div class="form-check @error('cgu') is-invalid @enderror">
                         <input class="form-check-input @error('cgu') is-invalid @enderror" type="checkbox" id="cgu" name="cgu" required/>
                         <label class="form-check-label" for="cgu">
-                            J’accepte les conditions générales d’utilisation.  <a href="{{ route('politique_de_confidentialite') }}">Lire</a>
+                            J’accepte les conditions générales d’utilisation.  <a class="text-primary" href="{{ route('politique_de_confidentialite') }}">Lire</a>
                         </label>
                         @error('cgu')
                             <div class="invalid-feedback d-block">
