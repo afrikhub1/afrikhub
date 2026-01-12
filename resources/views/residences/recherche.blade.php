dfa1559722242045045e056591a4e67ad41e6eee
Guess-dls <ninopaket@gmail.com>
2026-01-12 12:06:05 +0000

    Mise à jour du panier et suivi de commande

--- resources/views/residences/recherche.blade.php
+++ resources/views/residences/recherche.blade.php
@@ -188,7 +188,7 @@
     <header class="page-header text-center">
         <h1 class="fw-bold">Nos Résidences</h1>
         <p class="text-muted">Explorez nos meilleures offres de logements meublés</p>
-        <button class="btn btn-outline-dark" > <a class="d-flex text-decoration-none" href="{{route('accueil')}}">retour a l'accueil</a></button>
+         <a class="d-flex text-decoration-none" href="{{route('accueil')}}"><button class="btn btn-outline-dark" > Retour à l'accueil </button></a>
     </header>
 
     <div class="search-section">
