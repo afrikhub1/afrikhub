0ed931a7bf47728a571498e17bc9386051321259
Guess-dls <ninopaket@gmail.com>
2026-01-12 12:23:10 +0000

    Mise à jour du panier et suivi de commande

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -54,7 +54,7 @@
         <button type="submit" class="btn btn-primary w-100">Envoyer le lien</button>
     </form>
 
-    <a href="{{ url('/login') }}" class="d-block text-center mt-3 btn btn-light">← Retour à la connexion</a>
+    <a href="{{ url('/login') }}" class="d-block text-center mt-3 btn btn-dark">← Retour à la connexion</a>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
