de49db5f02c3ca592b63444522f4695a646d9123
Guess-dls <ninopaket@gmail.com>
2026-01-26 22:22:40 +0000

    Mise à jour du panier et suivi de commande

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -87,9 +87,7 @@
                         <div class="mt-2 p-3 bg-blue-50 border-l-4 border-blue-500 rounded text-sm text-blue-800">
                             <p class="font-bold mb-1">Sécurité du mot de passe :</p>
                             <ul class="list-disc list-inside space-y-1 ml-1">
-                                <li>Au moins 8 caractères</li>
-                                <li>Une lettre majuscule</li>
-                                <li>Un chiffre</li>
+                                <li>Au moins 8 caractères - Une lettre majuscule - Un chiffre</li>
                             </ul>
                         </div>
                         
