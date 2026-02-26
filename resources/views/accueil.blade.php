85ef987eaec36d7cc2a4bd57eacb65607839f738
afrikhub1 <afrikhub1@gmail.com>
2026-02-26 23:33:45 +0000

    Update accueil.blade.php

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -25,8 +25,10 @@
         <meta name="twitter:image" content="{{ asset('assets/images/og-image.png') }}">
     
         <!-- Favicons -->
-        <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon/favicon.ico') }}">
-        <link rel="icon" type="image/png" href="{{ asset('assets/favicon/favicon.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
  
     
         <!-- Styles -->
